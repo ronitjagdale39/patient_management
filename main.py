0d8e1a50ae1524265932b83a21a2dd7e97dec7d7
Ronit Jagdale <ronit.jagdale.39@gmail.com>
2025-12-17 22:21:44 +0530

    added main.py and patients.json

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI,HTTPException,Query
+from pydantic import BaseModel
+import json
+
+app2=FastAPI()
+
+filename='patients.json'
+def load_json():
+    with open(filename,'r') as f:
+        data1=json.load(f)
+        return data1
+@app2.get('/home')    
+def home():
+    return "helloooo frndssss"
+@app2.get('/patient/{patient_id}')
+def Show(patient_id:str):
+    data=load_json()
+    if patient_id not in data:
+        return HTTPException(status_code=400,detail="patients not found of this id ...")
+    else:
+        return data[patient_id]
+@app2.get('/sort')
+def Ordering(sort_by:str=Query(...,description='on what basisu wanna sort by : height,weight and bmi'),order:str=Query('asc',description="what do u wanna order it by ascending or descendding")):
+    valid_teams=['height','weight','bmi']
+    if sort_by not in valid_teams:
+        raise HTTPException(status_code=400,detail=f"enter btw {valid_teams}")
+    if order not in ['asc' ,'desc']:
+        raise HTTPException(status_code=400,detail='only asc or desc allowed ')    
